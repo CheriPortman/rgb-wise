7929c83e4194cfbd4d7572955d5ecc573a5af69d
Cheri Portman <cheriportman@hotmail.com>
2019-02-02 15:38:33 -0800

    have buttons with rgb set as value

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const swatchBox = document.getElementById('swatch-box');
+const buttonBox = document.getElementById('button-box');
 
 function randomRGB() {
     return Math.floor(Math.random() * Math.floor(255));
-    
 }
 
-function swatch() {
+function randomColor() {
     let r = randomRGB();
     let g = randomRGB();
     let b = randomRGB();
     let rgb = r + ',' + g + ',' + b;
-    console.log(rgb);
+    return rgb;
 }
 
-swatch();
+for(let i = 0; i < 6; i++) {
+    // console.log(randomColor());
+    const rgbButton = document.createElement('button');
+    rgbButton.value = randomColor();
+    rgbButton.textContent = 'This is it!'
+    console.log('rgb value', rgbButton.value);
+    buttonBox.appendChild(rgbButton);
 
-// lets start by randomly generating an rgb color code
+    //add event Listener
+}
 //draw 6 buttons
 //each button has a
 
 
 
 
-// for(let i = 0; i < 6; i++) {
-//     const swatch = document.createElement('button');
-
-
-// }
