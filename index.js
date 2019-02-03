a508b91e5f048a1822cb2bbaf3a9465f1e04a72a
Cheri Portman <cheriportman@hotmail.com>
2019-02-02 21:35:52 -0800

    commit before attempting to refactor a for loop into a function and still have everything work

--- index.js
+++ index.js
@@ -12,25 +12,30 @@ function randomColor() {
     return rgb;
 }
 
+function testMatch() {
+    console.log('inside testMatch()');            //need to figure out how to test 
+}
+
+//test if guessSwatch has the same value as targetSwatch
+    //this means I need to identify one of the swatches with an id of "target"
+
+
+
 for(let i = 0; i < 6; i++) {
     const rgbButton = document.createElement('button');
     const color = randomColor();
     
     rgbButton.style.backgroundColor = color;
-
     rgbButton.value = color;
-    rgbButton.classList.add('rgb-button');
-    rgbButton.style.backgroundColor = color;
-    
+    rgbButton.classList.add('rgb-button');    
     rgbButton.textContent = 'This is it!';
     
     buttonBox.appendChild(rgbButton);
 
-    //add event Listener
-
     rgbButton.addEventListener('click', function(event) {
         event.preventDefault();
         console.log('you clicked', rgbButton.value);
+        testMatch();
     });
 }
 
