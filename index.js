b59b44cded76e340226dbc7f5956fbf9635ef744
Cheri Portman <cheriportman@hotmail.com>
2019-02-02 17:08:13 -0800

    round buttons, working on getting colors set to random color value

--- index.js
+++ index.js
@@ -8,15 +8,18 @@ function randomColor() {
     let r = randomRGB();
     let g = randomRGB();
     let b = randomRGB();
-    let rgb = r + ',' + g + ',' + b;
+    let rgb = '(' + r + ',' + g + ',' + b + ')';
     return rgb;
 }
 
 for(let i = 0; i < 6; i++) {
     // console.log(randomColor());
     const rgbButton = document.createElement('button');
+    rgbButton.textContent = 'This is it!';
     rgbButton.value = randomColor();
-    rgbButton.textContent = 'This is it!'
+    rgbButton.classList.add('rgb-button');
+    rgbButton.style.backgroundColor = rgbButton.value;
+
     console.log('rgb value', rgbButton.value);
     buttonBox.appendChild(rgbButton);
 
