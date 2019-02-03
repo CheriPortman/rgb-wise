7ca4d025a2686630174a24e25a49fbe0df01a368
Cheri Portman <cheriportman@hotmail.com>
2019-02-03 10:58:40 -0800

    continued styling tweaks

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 This page is an exercise for practicing:
     <ul> <li> Manipulating the DOM</li> <li> Dynamically generating objects with random colors</li>
+
+text gradient code from: https://css-tricks.com/snippets/css/gradient-text/
+
--- index.js
+++ index.js
@@ -15,11 +15,12 @@ function randomColor() {
 }
 
 function restartOption() {
-    let restartButton = '<button id="reset" type="button">Try Again!</button>';
+    let restartButton = '<button id="reset" type="button">Play Again!</button>';
     revealBox.innerHTML = restartButton;
+    let reset = document.getElementById('reset');
     reset.addEventListener('click', function() {
         location.reload();
-    })
+    });
 }
 
 function setStage() {
@@ -30,26 +31,29 @@ function setStage() {
         const color = randomColor();
         
         if(i === randomIndex) {
-            rgbButton.setAttribute('id', 'target');
-            revealBox.innerHTML = color;
+            let rgbMessage = '<h4 id="rgb-message">Color to match:<h4>' + color;
+            revealBox.innerHTML = rgbMessage;
+            
+            
+            rgbButton.setAttribute('id', 'target');    
 
-            console.log('rgbButton:', rgbButton);
+            console.log('rgbButton:', rgbButton); 
         }
     
         rgbButton.style.backgroundColor = color;
         rgbButton.value = color;
         rgbButton.classList.add('rgb-button');    
         rgbButton.textContent = 'This is it!';
-        
         buttonBox.appendChild(rgbButton);
     
         rgbButton.addEventListener('click', function(event) {
             event.preventDefault();
             testMatch(rgbButton);
-            guess = guess + 1;
             
-            console.log('guesses:', guess);
-            
+            guess = guess + 1;   
+            const countBox = document.getElementById('count');    
+            const countMessage = '<h4>Guesses made so far:<h4>';
+            countBox.innerHTML = countMessage + guess;     
             if(guess > 2) {
                 let failImage = '<img id="fail"src=/assets/fail.png>';
                 buttonBox.innerHTML = failImage;
@@ -60,7 +64,6 @@ function setStage() {
 }
 
 function testMatch(rgbButton) {    
-
     if(rgbButton.id === 'target') {
         let swatches = document.getElementById('button-box');
         
@@ -69,9 +72,7 @@ function testMatch(rgbButton) {
             buttonBox.innerHTML = winImage;
             restartOption();
         }
-
     } else {
-        console.log(rgbButton);
         rgbButton.classList.add('hidden');
     }
 }
